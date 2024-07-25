077ef02f1af459828ef3439de827b677b9e6bc26
xavierc90 <colombelxavier7@gmail.com>
2024-07-25 23:38:21 +0200

    Ajout du composant BookingList pour le Dasboard

---
+++ src/Application/Components/BookingList.tsx
@@ -0,0 +1,8 @@
+export const BookingList = () => {
+  return (
+    <div>
+    <div>BookingList</div>
+    <h1>lklkdqsmldkqmlkqmldkqdqmlkdqmldkqmldksqlmdksqlmdkqlmdkqldkqlmkdlmdkl</h1>
+    </div>
+  )
+}
