bc94e6460f4eed24979507904424dc0b1a451200
jha <j.haimez@vattenfall.fr>
2024-07-25 11:22:33 +0200

    update : add component
    add button and calendar with shadcn

--- src/Application/Components/DashboardNav.tsx
+++ src/Application/Components/DashboardNav.tsx
@@ -1,15 +1,15 @@
 export const DashboardNav = () => {
+  
   return (
     <div className='bg-light w-3/12 h-screen flex flex-col pt-4 shadow-2xl'>
       <div className="flex justify-center">
-      <img src="../../../public/img/logo-oresto-red.png" width="280px" alt="Logo Oresto" />
+        <img src="../../../public/img/logo-oresto-red.png" width="280px" alt="Logo Oresto" />
       </div>
       
       <div>
-    <h2 className="text-lg font-bold mt-10 px-12 text-left">Recherche par date</h2>
-
-    </div>
-
+          <h2 className="text-lg font-bold mt-10 px-12 text-left">Recherche par date</h2>
+      </div>
+      
     </div>
   )
 }
