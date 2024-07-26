bd0f1ad07416b210f446ae6b3003903b8005a479
Xavier Colombel <colombelxavier7@gmail.com>
2024-07-26 15:31:06 +0200

    Test route interne composants

--- src/Application/Components/Widget/LoginFormUser.tsx
+++ src/Application/Components/Widget/LoginFormUser.tsx
@@ -1,7 +1,12 @@
-export const LoginFormUser = () => {
+import React, { Dispatch } from 'react'
+
+export const LoginFormUser: React.FC<{onClick: Dispatch<React.SetStateAction<number>>}> = ({onClick}) => {
+
+
+
 
   return (
-    <div className="flex flex-col justify-center items-center pt-5 pb-5 bg-white">
+    <div className="flex flex-col justify-center items-center pt-5 pb-5 px-6 bg-white">
         <img src="../../../public/img/logo-oresto-orange.png" width="250px" alt="Logo Oresto" />
         <form className="flex flex-col justify-center items-center mt-8">
             <div className="flex flex-col w-4/4">
@@ -15,7 +20,8 @@ export const LoginFormUser = () => {
             <button type="submit" className="bg-black rounded-lg text-white py-2 w-3/4 mt-6 mb-4 font-bold">Se connecter</button>
         <a href="#" className="hover:text-black">J'ai oublié mon mot de passe</a>
         </form>
-        <a className="bg-green-800 rounded-lg text-white py-2 w-3/4 text-center mt-4 font-bold hover:text-white">Créer un compte</a>
+        <a className="bg-green-800 rounded-lg text-white py-2 w-3/4 text-center mt-4 font-bold hover:text-white"
+        onClick={() => onClick(1)}>Créer un compte</a>
 </div>
   )
 }
