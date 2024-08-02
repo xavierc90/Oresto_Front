5e2e4afde93c8ab911ff33d060a2648c00426a93
Xavier Colombel <colombelxavier7@gmail.com>
2024-08-02 09:05:53 +0200

    Création d'une nouvelle branche pour débuter le drag and drop

--- src/Application/Pages/AnalyticsPage.tsx
+++ src/Application/Pages/AnalyticsPage.tsx
@@ -1,7 +1,7 @@
 export const AnalyticsPage = () => {
   return (
     <div className="bg-light w-full">
-    <h1 className="text-xl font-bold pt-4 pl-12">Statistiques</h1>
+    <h1 className="text-xl font-bold pt-2 pl-12">Statistiques</h1>
     <h2 className="text-lg pl-12 mt-1">
         <span className="font-bold text-red-500">0</span> réservation au total 
     </h2>
