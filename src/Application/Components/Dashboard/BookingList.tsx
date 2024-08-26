76c7e59a64b4ce537657f5f4949f0885d2699888
xavierc90 <colombelxavier7@gmail.com>
2024-08-26 07:53:02 +0200

    darkmode ajouté dans détail réservation

--- src/Application/Components/Dashboard/BookingList.tsx
+++ src/Application/Components/Dashboard/BookingList.tsx
@@ -3,6 +3,7 @@ import { FaSort, FaTimes } from "react-icons/fa";
 import { Booking } from '../../../Module/Types/bookng.type';
 import { http } from '../../../Infrastructure/Http/axios.instance';
 import { NotificationMessage } from '../NotificationMessage';
+import { RxCross1 } from 'react-icons/rx';
 
 interface BookingListProps {
   bookings: Booking[];
@@ -79,9 +80,9 @@ export const BookingList: React.FC<BookingListProps> = ({ bookings }) => {
               Heure <FaSort />
             </th>
             <th className="text-left min-w-[150px]">Nom</th>
+            <th className="text-left min-w-[150px]">Prénom</th>
             <th className="text-center min-w-[150px]">Nbr de couverts</th>
             <th className="text-center min-w-[180px]">Table</th>
-            <th className="text-center min-w-[180px]">Détails</th>
             <th className="text-center w-auto flex items-center justify-center gap-1">Statut<FaSort /></th>
           </tr>
         </thead>
@@ -94,9 +95,9 @@ export const BookingList: React.FC<BookingListProps> = ({ bookings }) => {
             >
               <td className="text-left">{booking.time_selected}</td>
               <td className="text-left">{booking.user_id.lastname}</td>
+              <td className="text-left">{booking.user_id.firstname}</td>
               <td className="text-center">{booking.nbr_persons} {booking.nbr_persons > 1 ? 'personnes' : 'personne'}</td>
               <td className="text-center">{booking.table && booking.table[0]?.table_number || 'N/A'}</td>
-              <td className="text-center">{booking.details || 'Aucun'}</td>
               <td className="text-center">
                 <span className={`px-7 py-1 text-sm font-semibold ${
                   booking.status === 'waiting' ? 'bg-orange-500 text-white' :
@@ -117,32 +118,35 @@ export const BookingList: React.FC<BookingListProps> = ({ bookings }) => {
 
       {/* Modal */}
       {isModalOpen && selectedBooking && (
-        <div 
+          <div 
           className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
           onClick={handleCloseModal}
-        >
+          >
           <div 
-            className="bg-white p-6 rounded-lg shadow-lg w-full max-w-lg relative"
+            className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg w-full max-w-lg relative"
             onClick={(e) => e.stopPropagation()}
           >
             {/* Close icon */}
             <button 
-              className="absolute top-2 right-2 text-gray-700 hover:text-black"
+              className="absolute top-4 right-4 text-gray-700 hover:text-black dark:text-white dark:hover:text-white"
               onClick={handleCloseModal}
             >
-              <FaTimes size={20} />
+              <RxCross1 size={25} />
             </button>
 
-            <h2 className="text-2xl font-bold mb-4 text-center">Détails de la réservation</h2>
-            <p><strong>Nom :</strong> {selectedBooking.user_id.firstname} {selectedBooking.user_id.lastname}</p>
-            <p><strong>Email :</strong> {selectedBooking.user_id.email}</p>
-            <p><strong>N° de téléphone :</strong> {selectedBooking.user_id.phone_number}</p>
-            <p><strong>Allergènes :</strong> {Array.isArray(selectedBooking.user_id.allergens) && selectedBooking.user_id.allergens.length > 0 ? selectedBooking.user_id.allergens.join(', ') : 'Aucune allergie signalée'}</p>            <p><strong>Heure :</strong> {selectedBooking.time_selected}</p>
-            <p><strong>Date :</strong> {new Date(selectedBooking.date_selected).toLocaleDateString()}</p>
-            <p><strong>Nombre de couverts :</strong> {selectedBooking.nbr_persons}</p>
-            <p><strong>Table :</strong> {selectedBooking.table && selectedBooking.table[0]?.table_number || 'N/A'}</p>
-            <p><strong>Détails :</strong> {selectedBooking.details || 'Aucun'}</p>
-            <div className="mt-8 flex justify-center gap-4">
+            <h2 className="text-2xl font-bold mb-6 mt-8 text-center">Détails de la réservation</h2>
+            <ul className="list-none pl-5 space-y-1">
+              <li><strong>Au nom de :</strong> {selectedBooking.user_id.firstname} {selectedBooking.user_id.lastname}</li>
+              <li><strong>Email :</strong> {selectedBooking.user_id.email}</li>
+              <li><strong>N° de téléphone :</strong> {selectedBooking.user_id.phone_number}</li>
+              <li><strong>Allergènes :</strong> {Array.isArray(selectedBooking.user_id.allergens) && selectedBooking.user_id.allergens.length > 0 ? selectedBooking.user_id.allergens.join(', ') : 'Aucune allergie signalée'}</li>
+              <li><strong>Heure :</strong> {selectedBooking.time_selected}</li>
+              <li><strong>Date :</strong> {new Date(selectedBooking.date_selected).toLocaleDateString()}</li>
+              <li><strong>Nombre de couverts :</strong> {selectedBooking.nbr_persons}</li>
+              <li><strong>Table :</strong> {selectedBooking.table && selectedBooking.table[0]?.table_number || 'N/A'}</li>
+              <li><strong>Détails :</strong> {selectedBooking.details || 'Aucun'}</li>
+            </ul>
+<div className="mt-8 flex justify-center gap-4">
               {selectedBooking.status !== 'confirmed' && (
                 <button className="bg-green-800 text-white font-semibold px-4 py-2 rounded-lg" onClick={handleConfirmBooking}>
                   Confirmer la réservation
--- src/Application/Pages/AnalyticsPage.tsx
+++ src/Application/Pages/AnalyticsPage.tsx
@@ -1,10 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { http } from "../../Infrastructure/Http/axios.instance";
+
 export const AnalyticsPage = () => {
+  const [totalBookings, setTotalBookings] = useState(0);
+  const [totalConfirmedBookings, settotalConfirmedBookings] = useState(0);
+  const [totalCanceledBookings, settotalCanceledBooking] = useState(0);
+
+  useEffect(() => {
+    const fetchTotalBookings = async () => {
+      try {
+        const response = await http.get('/bookings');
+        // Assurez-vous que la réponse contient un champ totalBookings
+        setTotalBookings(response.data.totalBookings);
+        settotalConfirmedBookings(response.data.totalConfirmedBookings);
+        settotalCanceledBooking(response.data.totalCanceledBooking);
+      } catch (error) {
+        console.error('Erreur lors de la récupération du nombre total de réservations:', error);
+      }
+    };
+
+    fetchTotalBookings();
+  }, []);
+
   return (
     <div className="bg-light w-full">
-    <h1 className="text-xl font-bold pt-8 pl-12">Statistiques</h1>
-    <h2 className="text-lg pl-12 mt-1">
-        <span className="font-bold text-red-500 dark:text-white">0</span> réservation au total 
-    </h2>
-</div>
-  )
-}
+      <h1 className="text-xl font-bold pt-8 pl-12">Statistiques</h1>
+      <h2 className="text-lg pl-12 mt-1">
+        <span className="font-bold text-red-500 dark:text-white">{totalBookings}</span> réservation{totalBookings > 1 ? 's' : ''} au total
+      </h2>
+      <ul className="pl-12 pt-12">
+      <li className="text-lg font-bold">Période annuelle</li>
+      <li>Réservations confirmées : {totalConfirmedBookings}</li>
+      <li>Réservations annulées : {totalCanceledBookings}</li>
+      </ul>
+    </div>
+  );
+};
